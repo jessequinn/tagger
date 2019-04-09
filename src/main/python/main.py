fe097c73c4bb22ba1a8ca04088f9ddfe70c596df
Jesse Quinn <me@jessequinn.info>
2019-04-09 14:11:12 -0300

    feat: unit testing added

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,18 +1,16 @@
 from fbs_runtime.application_context import ApplicationContext, cached_property
 from PyQt5.QtWidgets import QMainWindow, QFileSystemModel, QHeaderView, QListWidgetItem, QDialog
-from PyQt5.QtCore import QDir, QThread, pyqtSlot, Qt
+from PyQt5.QtCore import QDir, Qt
 
 import sys
 import os
 import ui
-import subprocess, shutil, re
-from tmdbv3api import TMDb, Movie, Genre, TV, Season
+from tagger import TaggerTV, TaggerMovie
+
+import subprocess
 from dotenv import load_dotenv
 
 load_dotenv()
-
-import pyAesCrypt
-from os import stat, remove
 
 
 class AppContext(ApplicationContext):
@@ -359,118 +357,6 @@ class MainWindow(QMainWindow, ui.Ui_MainWindow):
             pass
 
         return False
-
-
-class Tagger(object):
-    def __init__(self, key, language='en'):
-        self.tmdb = TMDb()
-        self.tmdb.api_key = key
-        self.tmdb.language = language
-
-
-class TaggerMovie(Tagger):
-    def __init__(self, key, language='en'):
-        Tagger.__init__(self, key, language)
-        self.movie = Movie()
-        self.genres = Genre().movie_list()
-
-    def __get_genres(self):
-        return dict([(g.id, g.name) for g in self.genres])
-
-    def search_results(self, title):
-        """
-        Search results for a movie
-        :param title:
-        :return: Returns an array that contains id, title, release date, overview, and first genre.
-        """
-        g = self.__get_genres()
-        arr = []
-
-        for item in self.movie.search(title):
-            title = item.title
-
-            try:
-                release_date = item.release_date.split('-')[0]
-            except IndexError:
-                release_date = ''
-
-            try:
-                overview = item.overview
-            except IndexError:
-                overview = ''
-
-            try:
-                first_genre = g[item.genre_ids[0]]
-            except IndexError:
-                first_genre = ''
-
-            arr.append([item.id, title, release_date, item.overview, first_genre])
-
-        return arr
-
-
-class TaggerTV(Tagger):
-    def __init__(self, key, language='en'):
-        Tagger.__init__(self, key, language)
-        self.tv = TV()
-
-    def search_results(self, show, season):
-        """
-        Search results for a season.
-        :param show:
-        :param season:
-        :return arr: Returns multidimensional array
-        """
-        results = self.tv.search(show)
-        arr = []
-
-        for item in results:
-            s = Season()
-            tv_details = self.tv.details(item.id)
-            show_name = tv_details.name
-            show_season = s.details(item.id, season)
-            seasons_amount = len(tv_details.seasons)
-
-            if season <= seasons_amount:
-                try:
-                    episode_titles = [episode['name'] for episode in
-                                      show_season.episodes]
-                except IndexError:
-                    episode_titles = []
-                except AttributeError:
-                    episode_titles = []
-
-                try:
-                    network = tv_details.networks[0]['name']
-                except IndexError:
-                    network = ''
-
-                try:
-                    episode_overviews = [episode['overview'] for episode in
-                                         show_season.episodes]
-                except IndexError:
-                    episode_overviews = []
-                except AttributeError:
-                    episode_overviews = []
-
-                try:
-                    episode_air_dates = [episode['air_date'] for episode in
-                                         show_season.episodes]
-                except IndexError:
-                    episode_air_dates = []
-                except AttributeError:
-                    episode_air_dates = []
-
-                try:
-                    first_genre = tv_details.genres[0]['name']
-                except IndexError:
-                    first_genre = ''
-
-                arr.append(
-                    [item.id, show_name, episode_titles, episode_air_dates, episode_overviews, first_genre, network,
-                     season, seasons_amount])
-
-        return arr
 
 
 if __name__ == '__main__':
