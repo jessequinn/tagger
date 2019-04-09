23297fe289e1b9cf75ef5861d569f77ce91e982a
Jesse Quinn <me@jessequinn.info>
2019-04-09 14:16:29 -0300

    fix: modified unit test for tagger

--- tests/taggertv_tests.py
+++ tests/taggertv_tests.py
@@ -5,7 +5,6 @@ Unit tests for Tagger.TaggerTV Class.
 """
 
 from unittest import TestCase
-from mock import patch
 import src.main.python.tagger as tagger
 
 
@@ -16,10 +15,16 @@ class StandAloneTests(TestCase):
 
     t = tagger.Tagger('keytest', 'pt')
 
-    @patch('__builtin__.open')
     def test_tagger_api_key(self):
         """
-        Test intial variables of an instance of Tagger
+        Test initial api key variables of an instance of Tagger.
         :return:
         """
-        self.assertEqual(t.tmdb.api_key, 'keytest')
+        self.assertEqual(self.t.tmdb.api_key, 'keytest')
+
+    def test_tagger_language(self):
+        """
+        Test initial language variable of an instance of Tagger.
+        :return:
+        """
+        self.assertEqual(self.t.tmdb.language, 'pt')
